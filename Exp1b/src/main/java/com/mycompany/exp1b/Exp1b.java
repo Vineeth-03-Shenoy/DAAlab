4914f39949a07ad2cdae91cab8cd117aa0304ccf
Vineeth-03-Shenoy <vineethshenoy22@gmail.com>
2022-08-15 10:36:43 +0530

    Experiment 1b is stacks, Yet to be programmed

--- Exp1b/src/main/java/com/mycompany/exp1b/Exp1b.java
+++ Exp1b/src/main/java/com/mycompany/exp1b/Exp1b.java
@@ -1,8 +1,8 @@
-
 package com.mycompany.exp1b;
 
-public class Exp1b {
+import java.util.Scanner;
 
+public class Exp1b {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
---
+++ Exp1b/src/main/java/com/mycompany/exp1b/Stack.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.exp1b;
+
+import java.util.Scanner;
+
+public class Stack {
+    private int n,top;
+    private int[] a;
+    public Stack(int n, int[] aq,int top){
+        
+    }
+}
