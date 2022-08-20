74a4f1378b09cd7672275e24f2d2231e4aa8bb43
Vineeth-03-Shenoy <20ai059.vineeth@sjec.ac.in>
2022-08-20 12:13:54 +0530

    Deleted files from filesystem

--- Exp1b/src/main/java/com/mycompany/exp1b/Exp1b.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.exp1b;
-
-import java.util.Scanner;
-
-public class Exp1b {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
